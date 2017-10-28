c38ef28da1605b5d8a4244f4cbcb2749536e81e6
Manfred_Muller_Lee <32573353+MullerLee@users.noreply.github.com>
2017-10-28 21:15:41 +0800

    Update S1_Attemp_Arduino.ino

--- S1_Attemp_Arduino.ino
+++ S1_Attemp_Arduino.ino
@@ -1,4 +1,32 @@
 # define S1 1
 # define S2 2
 # define S3 3
+# define S4 4
+# define OUT 5
 
+int count,time
+
+setup()
+{
+  digitalWrite(S1,INPUT);
+  digitalWrite(S2,INPUT);
+  digitalWrite(S3,INPUT);
+  digitalWrite(S4,INPUT);
+  digitalWrite(OUT,OUTPUT);
+  attachInterrupt(0,Control,FALLING);  //Interrupt Code, Function, Interrupt Mode
+}
+
+loop()
+{
+  
+  
+}
+
+void Control()
+{
+ 
+  count++;
+  if(count==10){ detachInterrupt(0);
+  }
+  attachInterrupt(0,Control,FALLING);
+}
